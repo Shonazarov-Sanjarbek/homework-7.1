8e70a181acaa4c4eeffcd363887b985cdd5bd446
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-06-14 23:07:23 +0500

    faqat js da masalalar ishlandi

---
+++ js/main.js
@@ -0,0 +1,345 @@
+// {
+//     // case1
+//     // function weekDay(n) {
+//     //     alert("1 dan 7 gacha butun sonlar kiritilgan. Kiritilgan sonlarga mos ravishda hafta kunlarini soz'da ifodalovchi programma tuzlsin")
+//     //     switch (n) {
+//     //         case 1:
+//     //             return `Dushanba`
+//     //         case 2:
+//     //             return `Seshanba`
+//     //         case 3:
+//     //             return `Chorshanba`
+//     //         case 4:
+//     //             return `Payshanba`
+//     //         case 5:
+//     //             return `Juma`
+//     //         case 6:
+//     //             return `Shanba`
+//     //         case 7:
+//     //             return `Yakshanba`
+//     //         default:
+//     //             return `bunday kun mavzud emas`
+//     //     }
+//     // }
+//     // console.log( weekDay(1) );
+//     // console.log( weekDay(2) );
+//     // console.log( weekDay(3) );
+//     // console.log( weekDay(4) );
+//     // console.log( weekDay(5) );
+//     // console.log( weekDay(6) );
+//     // console.log( weekDay(7) );
+//     // console.log( weekDay(8) );
+// }
+
+// {
+//     // case2
+//     // function result(n) {
+//     //     alert("k butun soni berilgan. Baho natijalarini chiqaruvchi programma tuzilsin")
+//     //     switch (n) {
+//     //         case 1:
+//     //             return `${1} - yomon`
+//     //         case 2:
+//     //             return `${2} - qoniqarsiz`
+//     //         case 3:
+//     //             return `${3} - qoniqarli`
+//     //         case 4:
+//     //             return `${4} - yaxshi`
+//     //         case 5:
+//     //             return `${5} - a'lo`
+//     //         default:
+//     //             return `xato`;
+//     //     }
+//     // }
+//     // console.log( result(1) );
+//     // console.log( result(2) );
+//     // console.log( result(3) );
+//     // console.log( result(4) );
+//     // console.log( result(5) );
+//     // console.log( result(6) );
+//     // console.log( result(0) );   
+// }
+
+// {
+//     // case3
+//     // function monthSeaon(n) {
+//     //     alert("Oy raqami berilgan. Kiritilgan oy qaysi faslga tegishli ekanligini chiqaruvchi programma tuzilsin.")
+//     //     switch (n) {
+//     //         case 1:
+//     //             return `birinchi oy - Qish`
+//     //         case 2:
+//     //         case 12:
+//     //         return `Qish`
+//     //         case 3:
+//     //         case 4:
+//     //         case 5:
+//     //         return `Bahor`
+//     //         case 6:
+//     //         case 7:
+//     //         case 8:
+//     //         return `Yoz`
+//     //         case 9:
+//     //         case 10:
+//     //         case 11:
+//     //         return `Kuz`
+//     //         default:
+//     //             return `bunday oy mavjud emas`
+//     //     }
+//     // }
+//     // console.log( monthSeaon(0) );
+//     // console.log( monthSeaon(1) );
+//     // console.log( monthSeaon(2) );
+//     // console.log( monthSeaon(3) );
+//     // console.log( monthSeaon(4) );
+//     // console.log( monthSeaon(5) );
+//     // console.log( monthSeaon(6) );
+//     // console.log( monthSeaon(7) );
+//     // console.log( monthSeaon(8) );
+//     // console.log( monthSeaon(9) );
+//     // console.log( monthSeaon(10) );
+//     // console.log( monthSeaon(11) );
+//     // console.log( monthSeaon(12) );
+//     // console.log( monthSeaon(13) );
+// }
+
+// {
+//     // case4
+//     // function monthDay(n) {
+//     //     alert("Oy raqami berilgan. Shu oyda nechta kun borligini aniqlovchi dastur tuzilsin.")
+//     //     switch (n) {
+//     //         case 1:
+//     //         case 3:
+//     //         case 5:
+//     //         case 7:
+//     //         case 8:
+//     //         case 10:
+//     //         case 12:
+//     //              return `${n} - oyda 31 kun bor `
+//     //         case 2:
+//     //             return `${n} - oyda 28 kun bor (4 yilda bir 29 kun bo'ladi)`   
+//     //         case 4:
+//     //         case 6:
+//     //         case 9:
+//     //         case 11:
+//     //             return `${n} oyda 30 kun bor`
+//     //         default:
+//     //             return ` bunday oy mavjud emas`
+//     //     }
+//     // }
+    
+//     // console.log( monthDay(0) );
+//     // console.log( monthDay(1) );
+//     // console.log( monthDay(2) );
+//     // console.log( monthDay(3) );
+//     // console.log( monthDay(4) );
+//     // console.log( monthDay(5) );
+//     // console.log( monthDay(6) );
+//     // console.log( monthDay(7) );
+//     // console.log( monthDay(9) );
+//     // console.log( monthDay(10) );
+//     // console.log( monthDay(11) );
+//     // console.log( monthDay(12) );
+//     // console.log( monthDay(13) );
+// }
+
+// {
+//     // case5
+//     // function number(a, b, amal) {
+//     //     alert("a va b haqiqiy va amal butun soni berilgan. a va b sonlari ustida arifmetik amallar bajaruvchi programma tuzilsin. amal quyidagi qiymatlarni qabul qiladi: \n 1 - qo'shish, 2 - aiyirish, 3 - bo'lish, 4 - ko'paytirish")
+    
+//     //     let bPlus = "qo'shish"
+//     //     let bMinus = "aiyirish"
+//     //     let bBoluv = "bo'lish"
+//     //     let bKopaytiruv = "ko'paytirish"
+//     //     switch (a, b, amal) {
+//     //         case 1:
+//     //         return a + b
+//     //         case 2:
+//     //         return a - b
+//     //         case 3:
+//     //             if (b !== 0) {
+//     //                 return a / b               
+//     //             }else{
+//     //                 return `0 ga bo'lib bo'lmaydi`
+//     //             }
+//     //         case 4:
+//     //         return a * b
+//     //         default:
+//     //             return `bunday amal yo'q`
+//     //     }
+//     // }
+    
+//     // console.log( `Natija: ${number (2, 1, 1)}. Kiritlgan sonlar a = ${2} b = ${1}. kiritilgan amal = ${1} - qo'shish`  );
+//     // console.log( `Natija: ${number (5, 3, 2)}. Kiritlgan sonlar a = ${5} b = ${3}. kiritilgan amal = ${2} - ayirish`  );
+//     // console.log( `Natija: ${number (8, 2, 3)}. Kiritlgan sonlar a = ${8} b = ${2}. kiritilgan amal = ${3} - bo'lish`  );
+//     // console.log( `Natija: ${number (8, 0, 3)}. Kiritlgan sonlar a = ${8} b = ${0}. kiritilgan amal = ${3} - bo'lish`  );
+//     // console.log( `Natija: ${number (8, 3, 4)}. Kiritlgan sonlar a = ${8} b = ${3}. kiritilgan amal = ${4} - ko'paytirish`  );
+//     // console.log( `Kiritlgan sonlar a = ${8} b = ${3}. kiritilgan amal = ${0} - c${number (8, 3, 0)}`  );
+// }
+
+// {
+//     // case6
+//     // function metr( m, length) {
+//     //     alert("uzunlik qiymatlari quyidagicha berilgan. 1 - detsimetr, 2 - kilometr, 3 - metr, 4 -  millimetr, 5 - santimetr. Uzunlik birligini bildiruvchi son berilgan (1 - 5 oraliqda ) va shu birlikdagi kesma uzunligini berilgan ( haqiqiy son ). Kesmaning uzunligini toping.")
+    
+//     //     switch (length) {
+//     //         case 1:
+//     //             return  `Natija: ${ m * 0.1} metr. Kiritilgan qiymat = ${ m } dm`   
+//     //         case 2:
+//     //             return  `Natija: ${ m * 1_000} metr. Kiritilgan qiymat = ${ m } km`
+//     //         case 3:
+//     //             return  `Natija: ${ m } metr. Kiritilgan qiymat = ${ m } m`
+//     //         case 4:
+//     //             return  `Natija: ${ m * 0.001} metr. Kiritilgan qiymat = ${ m } mm`
+//     //         case 5:
+//     //             return  `Natija: ${ m * 0.01} metr. Kiritilgan qiymat = ${ m } sm`
+//     //         default:
+//     //             return `Noto'g'ri birlik soni kiritildi. Iltimos 1 - 5 gacha bo'lgan birlikni tanlang.`
+//     //     }
+//     // }
+//     // console.log( metr(9, 1) );
+//     // console.log( metr(7, 2) );
+//     // console.log( metr(5, 3) );
+//     // console.log( metr(6, 4) );
+//     // console.log( metr(2, 5) );
+//     // console.log( metr(6, 6) );
+// }
+
+// {
+//     // case7
+//     // function massa( kg, weight ) {
+//     //     alert("Og'irlik birliklari quyidagi tartibda berilgan. 1 - kilogramm, 2- milligramm, 3 - gramm, 4 - tonna, 5 - sentner. Uzunlik birligini bildirtuvchi soni berilgan va shu birlikdagi og'irlik qiymati berilgan. Og'irlikni kilogramda ifodalovchi programma tuzilsin ")
+//     //     switch ( weight ) {
+//     //         case 1:
+//     //             return `Natija: ${ kg } kg. Kiritilgan qiymat = ${ kg } kg`
+//     //         case 2:
+//     //             return `Natija: ${ kg * 0.000_001 } kg. Kiritilgan qiymat = ${ kg } mg`
+//     //         case 3:
+//     //             return `Natija: ${ kg * 0.001 } kg. Kiritilgan qiymat = ${ kg } g`
+//     //         case 4:
+//     //             return `Natija: ${ kg * 1000 } kg. Kiritilgan qiymat = ${ kg } t`
+//     //         case 5:
+//     //             return `Natija: ${ kg * 100 } kg. Kiritilgan qiymat = ${ kg } st`
+//     //         default:
+//     //             return `Noto'g'ri birlik soni kiritildi. Iltimos 1 dan 5 gacha son kiriting`
+//     //     }
+//     // }
+    
+//     // console.log( massa(3, 1) );
+//     // console.log( massa(6, 2) );
+//     // console.log( massa(2, 3) );
+//     // console.log( massa(9, 4) );
+//     // console.log( massa(4, 5) );
+//     // console.log( massa(7, 6) );
+// }
+
+// {
+//     // case8 bu masalani ohirida koraman
+//     // function month(m) {
+//     //     switch (m) {
+//     //         case 1:
+//     //             return `Yanvar`
+//     //         case 2:
+//     //             return `Fevral`
+//     //         case 3:
+//     //             return `Mart`
+//     //         case 4:
+//     //             return `Aprel`
+//     //         case 5:
+//     //             return `May`
+//     //         case 6:
+//     //             return `Iyun`
+//     //         case 7:
+//     //             return `Iyul`
+//     //         case 8:
+//     //             return `Avgust`
+//     //         case 9:
+//     //             return `Sentabr`
+//     //         case 10:
+//     //             return `Oktabr`
+//     //         case 11:
+//     //             return `Noyabr`
+//     //         case 12:
+//     //             return `Dekabr`
+//     //         default:
+//     //             return `Bunday oy mavjud emas`
+//     //     }
+//     //     for (let i = 0; i < 31; i++) {
+//     //         const element = array[i];
+//     //         return i++
+//     //     }
+//     // }
+//     // console.log( month(2),  );
+// }
+
+
+
+
+
+
+
+
+
+
+
+// for
+
+// {
+//     // for1
+//     // function number( k, n ) {
+//     //     if ( n > 0) {
+//     //         for (let i = 0; i < n; i++) {
+//     //         console.log(k);            
+//     //         }       
+//     //     } else {
+//     //         return `n soni musbat bo'lishi kerak.`
+//     //     }
+//     // }
+    
+//     // console.log( number(2, 5) );
+// }
+
+// {
+//     // for2
+//     // function numbers(a, b) {
+//     //     let sum = 0
+//     //     if ( a < b) {
+//     //         for (let i = a; i <= b; i++) {
+//     //             console.log(i);          
+//     //         }
+//     //     } else {
+//     //         return `a soni b sonidan kichik bo'lishi kerak`
+//     //     }
+//     //     console.log(b - a + 1);
+//     // }
+    
+//     // console.log( numbers(3, 7 ) );
+// }
+
+
+// {
+//     // for3
+//     // function numbers( a, b ) {
+//     //     if ( a < b ) {
+//     //         for (let i = b -= 1; i > a; i--) {
+//     //             console.log(i);            
+//     //         }
+//     //     } else {
+//     //         return `a soni b dan kichik bo'lishi kerak`
+//     //     }
+//     //     console.log( b - a );
+//     // }
+    
+//     // console.log( numbers(1, 5) );
+// }
+
+{
+
+}
+
+function chocolate( prise ) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(prise * i);        
+    }
+}
+
+console.log( chocolate( 10000 ) );
